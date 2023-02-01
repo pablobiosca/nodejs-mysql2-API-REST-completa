b22038b5e6e2a5fe7cfab5ea1bf3511ee99994fc
chabaking <zaslake@gmail.com>
2023-02-01 13:57:27 +0100

    agregando endpoints

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use(express.static(path.join(__dirname, 'public')));
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("hola")
 });
+
+router.get('/employees', function(req, res, next) {
+  res.send("obteniendo empleados")
+});
+
+router.post('/employees', function(req, res, next) {
+  res.send("creando empleados")
+});
+
+router.put('/employees', function(req, res, next) {
+  res.send("actualizando empleado")
+});
+
+router.delete('/employees', function(req, res, next) {
+  res.send("eliminando empleados")
+});
+
 
 module.exports = router;
